468d0f6acbaa1e0dbccaf977ef9b6b3752b64962
s1f102301336 <s1f102301336@iniad.org>
2024-05-01 16:30:36 +0900

    コードを少し修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,39 +35,41 @@ const Home = () => {
       const x_around: number = r[1];
       let p = y_around * 2;
       let q = x_around * 2;
+      // if文で端のマスに置けなくならないように、*2
 
-      // となりが自分と違う色だったら
+      // 周りが自分と違う色だったら
       if (
         board[y][x] === 0 &&
         board[y + y_around] !== undefined &&
         board[y + y_around][x + x_around] === 3 - turnColor
       ) {
+        // 端まで繰り返す
         for (let i: number = 3; board[y + p] !== undefined; i++) {
           console.log('detect');
-
           console.log(p);
           console.log(q);
+          // 端まで来たor石がない時
           if (board[y + p][x + q] === undefined || board[y + p][x + q] === 0) {
             console.log('False');
             break;
           }
+          // 自分の石があったとき
           if (board[y + p][x + q] === turnColor) {
             console.log('True');
-            p /= i;
-            q /= i;
             console.log(i);
-            newBoard[y][x] = turnColor;
-            for (let j = 1; j < i; j++) {
+            // 置いたマスから見つけた石まで、ひっくり返す
+            for (let j = 0; j < i; j++) {
               console.log('Turn');
               p = y_around * j;
               q = x_around * j;
-
               newBoard[y + p][x + q] = turnColor;
             }
+            // ターン変更
             console.log('change');
             setTurnColor(3 - turnColor);
             break;
           }
+          // 繰り返しの更新
           p = y_around * i;
           q = x_around * i;
         }
@@ -82,6 +84,7 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.text}>a</div>
       <div className={styles.boardStyle}>
         {board.map((row, y) =>
           row.map((color, x) => (
